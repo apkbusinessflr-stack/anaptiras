23aba170ab195be87572829954e3737fe59a1a6e
apkbusinessflr-stack <apkbusinessflr@gmail.com>
2025-10-18 14:40:06 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-
 import type { Config } from 'tailwindcss'
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 }
